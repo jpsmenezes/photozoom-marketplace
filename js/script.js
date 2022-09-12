777088a7d6056f4bb60111b3c4fc5292c0a276b4
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2022-09-12 17:23:15 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 'use strict'
 
 //ABRIR OU FECHAR O MENU NA VERSÃO MOBILE
-function openMenu(){
+const openMenu = () => {
     document.body.classList.add('expended');
 }
-function closeMenu(){
+const closeMenu = () => {
     document.body.classList.remove('expended');
 }
 
 document.querySelector('.menu-open').addEventListener('click', openMenu);
 document.querySelector('.menu-close').addEventListener('click', closeMenu);
 
-
+//FECHAR O MENU MOBILE AO CLICAR EM QUALQUER LINK
 document.querySelectorAll('.linkMenu').forEach(link => link.addEventListener('click', closeMenu));
 document.querySelector('.nav-btn-login').addEventListener('click', closeMenu);
 
